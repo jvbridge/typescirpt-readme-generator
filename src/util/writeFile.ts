4e621e184d7a31a16ee0a8db5e78774a52ed071b
Jonathan Bridge <jvbridge@gmail.com>
2022-06-14 11:40:29 -0700

    able to write out a file

--- src/util/writeFile.ts
+++ src/util/writeFile.ts
@@ -1,12 +1,15 @@
 import * as fs from 'fs/promises'
 import * as path from 'path'
 
+const OUTPUT_PATH = "../output/"
 /**
  * Writes a file out to the appropriate directory
  * @param str the string to write to the file
  */
 async function writeFile(str: string, fileName: string){
-  await fs.writeFile(path.join(__dirname, fileName), str)
+  const output = OUTPUT_PATH + fileName;
+
+  await fs.writeFile(path.join(__dirname, output), str)
   console.log("successfully wrote to: ", fileName);
 }
 
