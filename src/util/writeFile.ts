62098e55c68f083bd496eb00f6bcb12bb5745ca3
Jonathan Bridge <jvbridge@gmail.com>
2022-06-14 13:04:08 -0700

    updated write file to check if the folder exists first

--- src/util/writeFile.ts
+++ src/util/writeFile.ts
@@ -1,12 +1,32 @@
-import * as fs from 'fs/promises'
-import * as path from 'path'
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
 
 const OUTPUT_PATH = "../output/"
+
+/**
+ * checks if a path exists
+ * @param checkPath the path we wish to check if it exists
+ * @returns 
+ */
+async function pathExists (checkPath: string): Promise<boolean>{
+  try {
+    await fs.access(checkPath); 
+    return true;
+  } catch{
+    return false;
+  }
+}
+
 /**
  * Writes a file out to the appropriate directory
  * @param str the string to write to the file
  */
 async function writeFile(str: string, fileName: string){
+  
+  if (!pathExists(OUTPUT_PATH)){
+    await fs.mkdir(OUTPUT_PATH);
+  }
+  
   const output = OUTPUT_PATH + fileName;
 
   await fs.writeFile(path.join(__dirname, output), str)
