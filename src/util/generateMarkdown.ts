c22acdbda3cd0ad2229b70f1dcc61727f1d9d0d6
Jonathan Bridge <jvbridge@gmail.com>
2022-06-15 10:45:02 -0700

    wer don't need to export the type

--- src/util/generateMarkdown.ts
+++ src/util/generateMarkdown.ts
@@ -2,7 +2,7 @@ import { Config } from './inquire';
 import licenses from './license.json';
 
 // creating a type for accessing the object
-export type licenseKey = keyof typeof licenses;
+type licenseKey = keyof typeof licenses;
 
 function generateMarkdown(config: Config): string {
   const { title, description, installation, usage, contributing, tests } =
