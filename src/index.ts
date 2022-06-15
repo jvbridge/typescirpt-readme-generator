8a563092b718e143ef66d99fb13369def77c5a92
Jonathan Bridge <jvbridge@gmail.com>
2022-06-14 20:50:17 -0700

    comments and changing output

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import configuration from './util/inquire';
 import generateMarkdown from './util/generateMarkdown';
 import { Config } from './util/inquire';
 
-const OUTPUT_FILE = 'test.txt';
-
 /**
  * The initializataion funciton for our generator
  */
 async function init() {
+  // get the configuration from the user
   const config:Config = await configuration();
   console.log('got configuration, now pulling from generate readme');
+
+  // generate the file into a single string
   const fileString = generateMarkdown(config);
-  await writeFile(fileString, OUTPUT_FILE);
+
+  // write it
+  await writeFile(fileString, config.filename);
+  console.log("Completed writing config to: ")
 }
 // run it
 init();
